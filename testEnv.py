ece3e6326f8f4031d042b53000551ffb12aae37d
ROMAIN BOQUET s332589 <s332589@studenti.polito.it>
2025-01-21 15:01:08 +0100

    feat: training

--- testEnv.py
+++ testEnv.py
@@ -1,7 +1,6 @@
 from SnakeEnv import SnakeEnv
 import time
 
-
 env = SnakeEnv()
 
 state = env.reset()
@@ -9,12 +8,8 @@ done = False
 
 while not done:
     action = env.action_space.sample()  # Action aléatoire
-    print(action)
-
     state, reward, done, info = env.step(action)
-
     env.render()
     time.sleep(0.1)
-    print(reward)
 
 env.close()
---
+++ training.py
@@ -0,0 +1,11 @@
+from stable_baselines3 import PPO
+from SnakeEnv import SnakeEnv
+
+# Créez l'environnement
+env = SnakeEnv(grid_size=15)
+
+# Créez un agent PPO
+model = PPO("CnnPolicy", env, verbose=1)
+
+# Entraînez l'agent
+model.learn(total_timesteps=10000)
