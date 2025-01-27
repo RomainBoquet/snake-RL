c1a6b35fb033a0fba69ce3a69ffecd724b0399ea
RomainBoquet <romain.boquett@gmail.com>
2025-01-27 18:49:59 +0100

    ajout du record video pendant l'entrainement + modification du modèle PPO

--- training.py
+++ training.py
@@ -1,13 +1,18 @@
 from stable_baselines3 import PPO
 from SnakeEnv import SnakeEnv
+from stable_baselines3.common.vec_env import VecVideoRecorder, DummyVecEnv
 
-# Créez l'environnement
-env = SnakeEnv(grid_size=15)
+env = DummyVecEnv([lambda: SnakeEnv(grid_size=10)])
+env = VecVideoRecorder(env, "videos/", record_video_trigger=lambda x: x % 100 == 0, video_length=200)
 
-# Créez un agent PPO
-model = PPO("MlpPolicy", env, verbose=1)
+model = PPO("CnnPolicy",
+            env,
+            learning_rate=1e-4,
+            n_steps=2048,
+            batch_size=64,
+            verbose=1)
 
 # Entraînez l'agent
 model.learn(total_timesteps=50000)
 
-model.save('models/ppo_snake_model')
+model.save('models/ppo_snake_model_cnn')
