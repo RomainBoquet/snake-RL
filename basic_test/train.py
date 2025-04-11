1d9cbeb8b14229f29f90cd9e3c6cb802075668c6
Romain <romain.boquett@gmail.com>
2025-04-11 11:36:14 +0200

    train basicEnv

---
+++ basic_test/train.py
@@ -0,0 +1,19 @@
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
+from basic_test.basicEnv import SimpleGameEnv
+
+
+env = SimpleGameEnv()
+vec_env = make_vec_env(lambda: env, n_envs=1)
+
+model = PPO('CnnPolicy', vec_env, verbose=1)
+model.learn(total_timesteps=10000)
+print("Training complete!")
+
+obs = vec_env.reset()
+for _ in range(1000):
+    action, _states = model.predict(obs)
+    obs, rewards, dones, info = vec_env.step(action)
+    # vec_env.render()
+
+print(rewards, dones, info)
